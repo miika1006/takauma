84c8e9480d78e24712e67c82248ab1ac0a4595b0
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-06-17 10:09:31 +0300

    added next-i18next locale
    Added locale for texts in ui

--- takauma/components/layout.tsx
+++ takauma/components/layout.tsx
@@ -1,16 +1,18 @@
-import Header from "./header"
-import Footer from "./footer"
-import styles from "./layout.module.css"
+import Header from "./header";
+import Footer from "./footer";
+import styles from "./layout.module.css";
+import { TFunction } from "next-i18next";
 interface LayoutProps {
-  children: React.ReactNode
+	children: React.ReactNode;
+	t: TFunction;
 }
 
-export default function Layout({ children }: LayoutProps) {
-  return (
-    <div className={styles.main}>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  )
+export default function Layout({ children, t }: LayoutProps) {
+	return (
+		<div className={styles.main}>
+			<Header t={t} />
+			<main>{children}</main>
+			<Footer />
+		</div>
+	);
 }
--- takauma/pages/index.tsx
+++ takauma/pages/index.tsx
@@ -1,15 +1,23 @@
-  
-import Layout from '../components/layout'
-import Image from 'next/image'
+import Layout from "../components/layout";
+import Image from "next/image";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { GetStaticProps } from "next";
 
-export default function Page () {
-  return (
-    <Layout>
-      <h1>Takauma</h1>
-      <Image src="/logo.svg" alt="Logo" width={100} height={100} />
-      <p>
-        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
-      </p>
-    </Layout>
-  )
+export default function Page() {
+	const { t } = useTranslation("common");
+
+	return (
+		<Layout t={t}>
+			<h1>{t("apptitle")}</h1>
+			<Image src="/logo.svg" alt="Logo" width={100} height={100} />
+			<p>{t("appdescription")}</p>
+		</Layout>
+	);
 }
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+	props: {
+		...(await serverSideTranslations(locale as string, ["common"])),
+	},
+});

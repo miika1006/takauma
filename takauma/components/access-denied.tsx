d452bb7d88c0e2d0d010f323f758b1f3e0af0b11
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-06-21 08:08:45 +0300

    localized texts

--- takauma/components/access-denied.tsx
+++ takauma/components/access-denied.tsx
@@ -1,16 +1,23 @@
-import { signIn } from 'next-auth/client'
-
-export default function AccessDenied () {
-  return (
-    <>
-      <h1>Access Denied</h1>
-      <p>
-        <a href="/api/auth/signin"
-           onClick={(e) => {
-           e.preventDefault()
-           signIn()
-        }}>You must be signed in to view this page</a>
-      </p>
-    </>
-  )
+import { signIn } from "next-auth/client";
+import { TFunction } from "next-i18next";
+interface AccessDeniedProps {
+	t: TFunction;
+}
+export default function AccessDenied({ t }: AccessDeniedProps) {
+	return (
+		<>
+			<h1>{t("accessdenied")}</h1>
+			<p>
+				<a
+					href={`/api/auth/signin`}
+					onClick={(e) => {
+						e.preventDefault();
+						signIn("google"); //Google, because it is only provider
+					}}
+				>
+					{t("googlesignin")}
+				</a>
+			</p>
+		</>
+	);
 }

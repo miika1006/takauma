921eb706a0093917d9b4ba5994a5db585cdc02ea
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-06-23 10:26:27 +0300

    folders and form to create new event

--- takauma/components/googledriveupload.tsx
+++ takauma/components/googledriveupload.tsx
@@ -1,11 +1,16 @@
+import { drive_v3 } from "googleapis";
 import { TFunction } from "next-i18next";
 import { useRef, useState } from "react";
 import styles from "../styles/googledriveupload.module.css";
 interface GoogleDriveUploadProps {
 	t: TFunction;
+	folders: drive_v3.Schema$File[];
 }
 
-export default function GoogleDriveUpload({ t }: GoogleDriveUploadProps) {
+export default function GoogleDriveUpload({
+	t,
+	folders,
+}: GoogleDriveUploadProps) {
 	const [image, setImage] = useState<File | null>(null);
 	const [eventName, setEventName] = useState<string>("");
 	const [createObjectURL, setCreateObjectURL] = useState<string>("");
@@ -18,7 +23,13 @@ export default function GoogleDriveUpload({ t }: GoogleDriveUploadProps) {
 			setCreateObjectURL(URL.createObjectURL(imagefile));
 		}
 	};
-
+	const createEvent = async (event: React.FormEvent<HTMLFormElement>) => {
+		try {
+		} catch (error) {
+			//TODO: Throw toast
+			console.error(error);
+		}
+	};
 	const uploadToServer = async (event: React.FormEvent<HTMLFormElement>) => {
 		try {
 			event.preventDefault();
@@ -34,26 +45,41 @@ export default function GoogleDriveUpload({ t }: GoogleDriveUploadProps) {
 			setImage(null);
 			if (fileInputRef?.current) fileInputRef.current.value = "";
 		} catch (error) {
+			//TODO: Throw toast
 			console.error(error);
 		}
 	};
 
 	return (
 		<div>
-			<form onSubmit={uploadToServer}>
-				{createObjectURL != "" && (
-					<img className={styles.thumbnail} alt="image" src={createObjectURL} />
-				)}
-				<h4>{t("event")}</h4>
+			{folders.map((folder, index) => {
+				<div key={folder.id}>{folder.name}</div>;
+			})}
+			<form onSubmit={createEvent}>
+				<h4>{t("createnewevent")}</h4>
 				<input
 					type="text"
 					value={eventName}
 					onChange={(e) => setEventName(e.target.value)}
 				/>
-				<h4>{t("selectimage")}</h4>
-				<input type="file" ref={fileInputRef} onChange={uploadToClient} />
-				<button type="submit">{t("upload")}</button>
+				<button type="submit">{t("save")}</button>
 			</form>
+			{eventName != "" && (
+				<form onSubmit={uploadToServer}>
+					{createObjectURL != "" && (
+						<img
+							className={styles.thumbnail}
+							alt="image"
+							src={createObjectURL}
+						/>
+					)}
+					<h4>{t("event")}</h4>
+					<input type="hidden" value={eventName} />
+					<h4>{t("selectimage")}</h4>
+					<input type="file" ref={fileInputRef} onChange={uploadToClient} />
+					<button type="submit">{t("upload")}</button>
+				</form>
+			)}
 		</div>
 	);
 }

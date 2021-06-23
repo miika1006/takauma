4a805caa2030a33989c8f867f962a003c80b51fa
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-06-23 09:54:19 +0300

    some texts and clear by ref

--- takauma/components/googledriveupload.tsx
+++ takauma/components/googledriveupload.tsx
@@ -1,5 +1,5 @@
 import { TFunction } from "next-i18next";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import styles from "../styles/googledriveupload.module.css";
 interface GoogleDriveUploadProps {
 	t: TFunction;
@@ -9,6 +9,7 @@ export default function GoogleDriveUpload({ t }: GoogleDriveUploadProps) {
 	const [image, setImage] = useState<File | null>(null);
 	const [eventName, setEventName] = useState<string>("");
 	const [createObjectURL, setCreateObjectURL] = useState<string>("");
+	const fileInputRef = useRef<HTMLInputElement>(null);
 
 	const uploadToClient = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.files && event.target.files[0]) {
@@ -22,6 +23,7 @@ export default function GoogleDriveUpload({ t }: GoogleDriveUploadProps) {
 		try {
 			event.preventDefault();
 			if (image == null) return;
+			if (eventName === "") return;
 			const body = new FormData();
 			body.append("file", image);
 			body.append("eventName", eventName);
@@ -29,23 +31,27 @@ export default function GoogleDriveUpload({ t }: GoogleDriveUploadProps) {
 				method: "POST",
 				body,
 			});
+			setImage(null);
+			if (fileInputRef?.current) fileInputRef.current.value = "";
 		} catch (error) {
 			console.error(error);
 		}
 	};
+
 	return (
 		<div>
 			<form onSubmit={uploadToServer}>
 				{createObjectURL != "" && (
 					<img className={styles.thumbnail} alt="image" src={createObjectURL} />
 				)}
-				<h4>{t("selectimage")}</h4>
+				<h4>{t("event")}</h4>
 				<input
 					type="text"
 					value={eventName}
 					onChange={(e) => setEventName(e.target.value)}
 				/>
-				<input type="file" onChange={uploadToClient} />
+				<h4>{t("selectimage")}</h4>
+				<input type="file" ref={fileInputRef} onChange={uploadToClient} />
 				<button type="submit">{t("upload")}</button>
 			</form>
 		</div>

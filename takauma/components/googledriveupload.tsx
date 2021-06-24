fd5bfc0a3b94033224ec0483f36fee2c1d41e0df
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-06-24 12:50:50 +0300

    added create folder and update files and folders

--- takauma/components/googledriveupload.tsx
+++ takauma/components/googledriveupload.tsx
@@ -1,6 +1,6 @@
 import { drive_v3 } from "googleapis";
 import { TFunction } from "next-i18next";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/googledriveupload.module.css";
 interface GoogleDriveUploadProps {
 	t: TFunction;
@@ -12,9 +12,38 @@ export default function GoogleDriveUpload({
 	folders,
 }: GoogleDriveUploadProps) {
 	const [image, setImage] = useState<File | null>(null);
-	const [eventName, setEventName] = useState<string>("");
+	const [currentEventName, setCurrentEventName] = useState<string>("");
+	const [createEventName, setCreateEventName] = useState<string>("");
 	const [createObjectURL, setCreateObjectURL] = useState<string>("");
 	const fileInputRef = useRef<HTMLInputElement>(null);
+	const [driveFiles, setDriveFiles] = useState<drive_v3.Schema$File[]>([]);
+	const [driveFolders, setDriveFolder] =
+		useState<drive_v3.Schema$File[]>(folders);
+
+	useEffect(() => {
+		getFiles(currentEventName);
+	}, [currentEventName]);
+
+	const getFiles = async (eventName: string) => {
+		try {
+			if (eventName === "") return;
+			const response = await fetch(
+				`/api/file?eventname=${encodeURIComponent(eventName)}`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Accept: "application/json",
+					},
+					method: "GET",
+				}
+			).then((r) => r.json());
+			console.log("getFiles response", response);
+			setDriveFiles(response);
+		} catch (error) {
+			//TODO: Throw toast
+			console.error(error);
+		}
+	};
 
 	const uploadToClient = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.files && event.target.files[0]) {
@@ -24,48 +53,92 @@ export default function GoogleDriveUpload({
 		}
 	};
 	const createEvent = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 		try {
+			if (createEventName === "") return;
+			const response = await fetch("/api/folder", {
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+				method: "POST",
+				body: JSON.stringify({ name: createEventName }),
+			}).then((r) => r.json());
+			console.log("createEvent response", response);
+			setCreateEventName("");
+			setDriveFolder((current) => [...current, response]);
+			setCurrentEventName(response.name);
 		} catch (error) {
 			//TODO: Throw toast
 			console.error(error);
 		}
 	};
-	const uploadToServer = async (event: React.FormEvent<HTMLFormElement>) => {
+	const upload = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
 		try {
-			event.preventDefault();
 			if (image == null) return;
-			if (eventName === "") return;
+			if (currentEventName === "") return;
 			const body = new FormData();
 			body.append("file", image);
-			body.append("eventName", eventName);
+			body.append("eventName", currentEventName);
 			const response = await fetch("/api/file", {
 				method: "POST",
 				body,
-			});
+			}).then((r) => r.json());
 			setImage(null);
+			console.log("upload response", response);
+			setDriveFiles((current) => [...current, response]);
+			setCreateObjectURL("");
 			if (fileInputRef?.current) fileInputRef.current.value = "";
 		} catch (error) {
 			//TODO: Throw toast
 			console.error(error);
 		}
 	};
-
+	const selectEvent = (foldername: string) => () => {
+		setCurrentEventName(foldername);
+	};
 	return (
-		<div>
-			{folders.map((folder, index) => {
-				<div key={folder.id}>{folder.name}</div>;
-			})}
+		<>
+			{driveFolders.map((folder, index) => (
+				<div key={folder.id}>
+					{folder.name}
+					{folder.name !== currentEventName && (
+						<button onClick={selectEvent(folder.name ?? "")}>
+							{t("select")}
+						</button>
+					)}
+				</div>
+			))}
 			<form onSubmit={createEvent}>
 				<h4>{t("createnewevent")}</h4>
 				<input
 					type="text"
-					value={eventName}
-					onChange={(e) => setEventName(e.target.value)}
+					value={createEventName}
+					onChange={(e) => setCreateEventName(e.target.value)}
 				/>
 				<button type="submit">{t("save")}</button>
 			</form>
-			{eventName != "" && (
-				<form onSubmit={uploadToServer}>
+			<hr />
+			{driveFiles.map((file, index) => (
+				<img
+					key={`thumb-${file.id}`}
+					className={styles.thumbnail}
+					alt={`image ${file.id ?? ""}`}
+					/*src={`https://www.googleapis.com/drive/v3/files/${
+							file.id ?? ""
+						}/export?mimeType=${file.mimeType}`}*/
+					//src={file.webContentLink ?? ""}
+					src={file.thumbnailLink ?? ""}
+
+					//TODO: Google cannot keep up, and crashes
+				/>
+			))}
+
+			{currentEventName != "" && (
+				<form onSubmit={upload}>
+					<input type="hidden" value={currentEventName} />
+					<h4>{t("selectimage")}</h4>
 					{createObjectURL != "" && (
 						<img
 							className={styles.thumbnail}
@@ -73,13 +146,10 @@ export default function GoogleDriveUpload({
 							src={createObjectURL}
 						/>
 					)}
-					<h4>{t("event")}</h4>
-					<input type="hidden" value={eventName} />
-					<h4>{t("selectimage")}</h4>
 					<input type="file" ref={fileInputRef} onChange={uploadToClient} />
 					<button type="submit">{t("upload")}</button>
 				</form>
 			)}
-		</div>
+		</>
 	);
 }

229b609c92f77e2f89914ce0b6a42c516180d7aa
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-06-28 22:29:26 +0300

    Removed file editing on event creator
    All photos are uploaded on event page with sharelink.

--- takauma/components/googledriveupload.tsx
+++ takauma/components/googledriveupload.tsx
@@ -1,8 +1,8 @@
 import { drive_v3 } from "googleapis";
 import { TFunction } from "next-i18next";
 import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
-import styles from "../styles/googledriveupload.module.css";
+import { useEffect, useState } from "react";
+
 interface GoogleDriveUploadProps {
 	t: TFunction;
 	folders: drive_v3.Schema$File[];
@@ -12,14 +12,11 @@ export default function GoogleDriveUpload({
 	t,
 	folders,
 }: GoogleDriveUploadProps) {
-	const [image, setImage] = useState<File | null>(null);
-	const [currentEvent, setCurrentEvent] =
-		useState<drive_v3.Schema$File | null>(null);
+	const [currentEvent, setCurrentEvent] = useState<drive_v3.Schema$File | null>(
+		null
+	);
 	const [createEventName, setCreateEventName] = useState<string>("");
-	const [createObjectURL, setCreateObjectURL] = useState<string>("");
 	const [shared, setShared] = useState<boolean>(false);
-	const fileInputRef = useRef<HTMLInputElement>(null);
-	const [driveFiles, setDriveFiles] = useState<drive_v3.Schema$File[]>([]);
 	const [driveFolders, setDriveFolders] =
 		useState<drive_v3.Schema$File[]>(folders);
 	const [shareUrl, setShareUrl] = useState<string>();
@@ -27,7 +24,6 @@ export default function GoogleDriveUpload({
 
 	useEffect(() => {
 		if (currentEvent) {
-			getFiles(currentEvent?.name ?? "");
 			setShared(currentEvent.shared ?? false);
 			setShareUrl(
 				window?.location?.origin + router.pathname + "/" + currentEvent?.id
@@ -35,34 +31,15 @@ export default function GoogleDriveUpload({
 		}
 	}, [currentEvent]);
 
-	const getFiles = async (eventName: string) => {
-		try {
-			if (eventName === "") return;
-			const response = await fetch(
-				`/api/file?eventname=${encodeURIComponent(eventName)}`,
-				{
-					headers: {
-						"Content-Type": "application/json",
-						Accept: "application/json",
-					},
-					method: "GET",
-				}
-			).then((r) => r.json());
-			console.log("getFiles response", response);
-			setDriveFiles(response);
-		} catch (error) {
-			//TODO: Throw toast
-			console.error(error);
-		}
-	};
-
-	const setToPagePreview = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files && event.target.files[0]) {
-			const imagefile = event.target.files[0];
-			setImage(imagefile);
-			setCreateObjectURL(URL.createObjectURL(imagefile));
-		}
-	};
+	/**
+	 * Creating new event
+	 * Create new event with given createEventName value
+	 * 1. Call Api to create event
+	 * 2. Server creates new folder to Google Drive and shares it to service account
+	 * 3. Returns result
+	 * @param event
+	 * @returns
+	 */
 	const createEvent = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		try {
@@ -79,6 +56,7 @@ export default function GoogleDriveUpload({
 				method: "POST",
 				body: JSON.stringify({ name: createEventName }),
 			}).then((r) => r.json());
+
 			console.log("createEvent response", response);
 			setCreateEventName("");
 
@@ -92,47 +70,25 @@ export default function GoogleDriveUpload({
 			console.error(error);
 		}
 	};
-	const upload = async (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		try {
-			if (image == null) return;
-			if (currentEvent === null || currentEvent.name === "") return;
 
-			const body = new FormData();
-			body.append("file", image);
-			body.append("eventName", currentEvent.name ?? "");
-			const response = await fetch("/api/file", {
-				method: "POST",
-				body,
-			}).then((r) => r.json());
-			setImage(null);
-			console.log("upload response", response);
-			setDriveFiles((current) => [...current, response]);
-			setCreateObjectURL("");
-			if (fileInputRef?.current) fileInputRef.current.value = "";
-		} catch (error) {
-			//TODO: Throw toast
-			console.error(error);
-		}
-	};
 	/**
-	  TODO:
-			1. Call api with folderId to share
-			2. On server side, share current folder by id to service account 
-			3. Create app sharelink that is accessable without authentication
-			   Link contains information: folderId that is shared and some salt?
-			4. A user opens the link, link contains parameter
-			5. Server gets parameter, extracts folderId from it
-			6. Server loads folder and containing images to page using service account with shared rights
-			7. A user now can view images without auth
-			8. A user now can upload new image without auth
-			   Image is received on serverside and uploaded to given folderId using service account with shared rights to folder
-			
-			TODO: Handle exceptions
-			- Folder is not found by folderid, show error/info on sharelink
-			- Folder is unshared
-			  Remove share from service account?
-	 * @param event 
+	 *  Sharing and what happens after that?
+	 * 	1. Call api with folderId to share and true|false if sharing
+	 *	2. On server side, share current folder by id to anyone
+	 *	3. Create app sharelink that is accessable without authentication
+	 *	   Link contains information: folderId that is shared and some salt?
+	 *	4. A user opens the link, link contains parameter(s)
+	 *	5. Server gets parameter(s), extracts folderId from it
+	 *	6. Server loads folder and containing images to page using service account
+	 *	7. A user now can view images without auth
+	 *	8. A user now can upload new image without auth
+	 *	   Image is received on serverside and uploaded to given folderId using service account with shared rights to folder
+	 *
+	 *	TODO: Handle exceptions
+	 *	- Folder is not found by folderid, show error/info on sharelink
+	 *	- Folder is unshared
+	 *	  Remove share from service account?
+	 * @param event
 	 */
 	const onShare = async (event: React.ChangeEvent<HTMLInputElement>) => {
 		const newSharedState = !shared;
@@ -158,6 +114,17 @@ export default function GoogleDriveUpload({
 			setShared(!newSharedState);
 		}
 	};
+
+	/**
+	 * Deleting evnt
+	 * 1. Call api with folderId to delete
+	 * 2. Server deletes Google Drive folder
+	 * 3. If folder contains some files uploaded with service account.
+	 *    Delete will fail.
+	 *    User should go to Google Drive and do cleanup there if needed.
+	 * @param folder (id,name)
+	 * @returns
+	 */
 	const deleteEvent = (folder: drive_v3.Schema$File) => async () => {
 		try {
 			const response = await fetch("/api/folder", {
@@ -179,6 +146,11 @@ export default function GoogleDriveUpload({
 			console.error(error);
 		}
 	};
+	/**
+	 * Set a event as active and selected for editing/sharing
+	 * @param folder
+	 * @returns
+	 */
 	const selectEvent = (folder: drive_v3.Schema$File) => () => {
 		setCurrentEvent(folder);
 	};
@@ -222,38 +194,6 @@ export default function GoogleDriveUpload({
 			<a href={shareUrl} target="_blank" rel="noopener noreferrer">
 				Avaa linkki ja aloita lataamaan kuvia
 			</a>
-
-			<br />
-			<br />
-			{driveFiles.map((file) => (
-				<img
-					key={`thumb-${file.id}`}
-					className={styles.thumbnail}
-					alt={`image ${file.id ?? ""}`}
-					//src={`https://www.googleapis.com/drive/v3/files/${
-					//		file.id ?? ""
-					//	}/export?mimeType=${file.mimeType}`}
-					//src={file.webContentLink ?? ""}
-					src={file.thumbnailLink ?? ""}
-
-					//TODO: Google cannot keep up, and crashes
-				/>
-			))}
-
-			{currentEvent && (
-				<form onSubmit={upload}>
-					<h4>{t("selectimage")}</h4>
-					{createObjectURL != "" && (
-						<img
-							className={styles.thumbnail}
-							alt="image"
-							src={createObjectURL}
-						/>
-					)}
-					<input type="file" ref={fileInputRef} onChange={setToPagePreview} />
-					<button type="submit">{t("upload")}</button>
-				</form>
-			)}
 		</>
 	);
 }

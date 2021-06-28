32c08cffb2b35b8f901c10335ed0b553545f57eb
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-06-28 21:42:47 +0300

    service account

--- takauma/types/environment.d.ts
+++ takauma/types/environment.d.ts
@@ -1,8 +1,10 @@
 namespace NodeJS {
-    interface ProcessEnv extends NodeJS.ProcessEnv {
-      NEXTAUTH_URL: string
-      SECRET: string
-      GOOGLE_ID: string
-      GOOGLE_SECRET: string
-    }
-  }
+	interface ProcessEnv extends NodeJS.ProcessEnv {
+		NEXTAUTH_URL: string;
+		SECRET: string;
+		GOOGLE_ID: string;
+		GOOGLE_SECRET: string;
+		SERVICE_ACCOUNT: string;
+		SERVICE_ACCOUNT_KEY: string;
+	}
+}

8954dc004cda3cfd0d34510a51d59f3ca2658ad1
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-07-03 09:05:22 +0300

    Added aws dynamo-db

--- takauma/types/environment.d.ts
+++ takauma/types/environment.d.ts
@@ -6,5 +6,10 @@ namespace NodeJS {
 		GOOGLE_SECRET: string;
 		SERVICE_ACCOUNT: string;
 		SERVICE_ACCOUNT_KEY: string;
+		AWS_ACCESS_KEY: string;
+		AWS_SECRET_KEY: string;
+		AWS_REGION: string;
+		AWS_TABLE_NAME: string;
+		AWS_NEXT_AUTH_TABLE_NAME: string;
 	}
 }

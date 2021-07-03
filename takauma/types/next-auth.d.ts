bcf6fec738d2e6473f7d5ffb5ccba65dc2079ebb
miika1006 <18673835+miika1006@users.noreply.github.com>
2021-07-03 09:04:41 +0300

    Extended next-auth types

--- takauma/types/next-auth.d.ts
+++ takauma/types/next-auth.d.ts
@@ -1,18 +1,31 @@
-import { Session } from "next-auth"
-import { JWT } from "next-auth/jwt"
+import { Session, User, DefaultUser } from "next-auth";
+//import { DefaultJWT } from "next-auth/jwt";
 
 /** Example on how to extend the built-in session types */
 declare module "next-auth" {
-  interface Session {
-    /** This is an example. You can find me in types/next-auth.d.ts */
-  //  foo: string
-  }
+	interface Session {
+		//extends Record<string, unknown>, DefaultSession {
+		accessToken: string;
+		error: string;
+	}
+	/*interface JWT extends Record<string, unknown>, DefaultJWT {
+		accessToken: string;
+		error: string;
+	}*/
+	interface User {
+		accessToken: string;
+		error: string;
+	}
+	interface Profile {
+		verified_email: boolean;
+	}
 }
 
-/** Example on how to extend the built-in types for JWT */
 declare module "next-auth/jwt" {
-  interface JWT {
-    /** This is an example. You can find me in types/next-auth.d.ts */
-   // bar: number
-  }
+	interface JWT {
+		accessToken: string;
+		refreshToken: string;
+		error: string;
+		accessTokenExpires: number;
+	}
 }

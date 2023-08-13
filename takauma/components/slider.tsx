759fbdea44920ebb51d7d17edc0ad00e02f9977d
miika1006 <18673835+miika1006@users.noreply.github.com>
2023-08-13 07:56:07 +0300

    optimized performance with colcade image grid
    Set initial items to grid from prop and initialize colcade. Then if items change, filter new items from prop compared to preivous and then append html nodes to colcade to redraw

--- takauma/components/slider.tsx
+++ takauma/components/slider.tsx
@@ -4,6 +4,8 @@ import "photoswipe/dist/photoswipe.css";
 import styles from "../styles/slider.module.css";
 import { Gallery, Item } from "react-photoswipe-gallery";
 import { isMobile } from "react-device-detect";
+import usePrevious from "../common/hooks/use-previous";
+import ReactDOMServer from "react-dom/server";
 
 interface SliderProps {
 	t: TFunction;
@@ -18,15 +20,28 @@ export interface SliderItem {
 		height?: number;
 	};
 }
+export interface ImageItemProps {
+	item: SliderItem;
+}
 
 interface Colcade {
 	new (element: string, options: any): Object;
 	option: (options: any) => void;
 	reload: () => void;
+	append: (items: any[]) => void;
+	prepend: (items: any[]) => void;
+}
+function htmlToElement(html: string) {
+	var template = document.createElement("template");
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
 }
 export default function Slider({ t, items }: SliderProps) {
+	const [initialItems, setInitialItems] = useState<SliderItem[]>(items);
 	const colcade = useRef<Colcade | null>(null);
 	const timer = useRef<number | null>(null);
+	const previousItems = usePrevious<SliderItem[]>(items);
 	useEffect(() => {
 		const loadColcade = async () => {
 			const Colcade: any = await import("colcade").then(
@@ -40,9 +55,10 @@ export default function Slider({ t, items }: SliderProps) {
 				items: `.${styles.griditem}`,
 			});
 		};
+		if (previousItems?.length === 0) setInitialItems(items);
 		if (!isMobile && colcade.current === null && items.length > 0)
 			loadColcade();
-	}, [items]);
+	}, [items, previousItems?.length]);
 
 	useEffect(() => {
 		if (isMobile) return;
@@ -50,29 +66,65 @@ export default function Slider({ t, items }: SliderProps) {
 
 		timer.current = window.setTimeout(() => {
 			if (colcade.current && items && items.length > 0) {
-				console.log("reloading colcade");
-				colcade.current.reload();
+				const newItems = items
+					.filter(
+						(newItem) =>
+							!(previousItems ?? []).some(
+								(previousItem) =>
+									previousItem.id === newItem.id &&
+									previousItem.thumbnailLink === newItem.thumbnailLink
+							)
+					)
+					.map((i) =>
+						htmlToElement(
+							ReactDOMServer.renderToStaticMarkup(
+								<ImageItem key={`photo_${i.id}_${i.thumbnailLink}`} item={i} />
+							)
+						)
+					);
+				console.log("appending items to colcade", newItems);
+
+				colcade.current.append(newItems);
 			}
-		}, 2000);
+		}, 100);
 
 		return () => {
 			if (timer.current) window.clearTimeout(timer.current);
 		};
-	}, [items]);
+	}, [items, previousItems]);
 
-	return items.length > 0 ? (
-		isMobile ? (
-			<ImageGallery t={t} items={items} />
-		) : (
-			<div className={styles.grid}>
-				<div className={styles.gridcol + " " + styles.gridcolone}></div>
-				<div className={styles.gridcol + " " + styles.gridcoltwo}></div>
-				<div className={styles.gridcol + " " + styles.gridcolthree}></div>
-				<div className={styles.gridcol + " " + styles.gridcolfour}></div>
-				<ImageGallery t={t} items={items} />
-			</div>
-		)
-	) : null;
+	return isMobile ? (
+		<ImageGallery t={t} items={items} />
+	) : (
+		<div className={styles.grid}>
+			<div className={styles.gridcol + " " + styles.gridcolone}></div>
+			<div className={styles.gridcol + " " + styles.gridcoltwo}></div>
+			<div className={styles.gridcol + " " + styles.gridcolthree}></div>
+			<div className={styles.gridcol + " " + styles.gridcolfour}></div>
+			<ImageGallery t={t} items={initialItems} />
+		</div>
+	);
+}
+export function ImageItem({ item }: ImageItemProps) {
+	return (
+		<Item
+			id={item.id ?? ""}
+			original={item.webContentLink ?? ""}
+			thumbnail={item.thumbnailLink ?? ""}
+			width={item.imageMediaMetadata?.width}
+			height={item.imageMediaMetadata?.height}
+		>
+			{({ ref, open }) => (
+				<img
+					className={styles.griditem}
+					alt={`photo_${item.id}_${item.thumbnailLink}_thumb`}
+					ref={ref as React.RefObject<HTMLImageElement>}
+					onClick={open}
+					src={item.thumbnailLink ?? ""}
+				/>
+			)}
+		</Item>
+	);
 }
 
 export function ImageGallery({ t, items }: SliderProps) {
@@ -87,25 +139,8 @@ export function ImageGallery({ t, items }: SliderProps) {
 			}}
 			withDownloadButton={true}
 		>
-			{items.map((item, idx) => (
-				<Item
-					id={item.id ?? ""}
-					key={`photo_${item.id}_${item.thumbnailLink}`}
-					original={item.webContentLink ?? ""}
-					thumbnail={item.thumbnailLink ?? ""}
-					width={item.imageMediaMetadata?.width}
-					height={item.imageMediaMetadata?.height}
-				>
-					{({ ref, open }) => (
-						<img
-							className={styles.griditem}
-							alt={`photo_${idx}_thumb`}
-							ref={ref as React.RefObject<HTMLImageElement>}
-							onClick={open}
-							src={item.thumbnailLink ?? ""}
-						/>
-					)}
-				</Item>
+			{items.map((item) => (
+				<ImageItem item={item} key={`photo_${item.id}_${item.thumbnailLink}`} />
 			))}
 		</Gallery>
 	);
